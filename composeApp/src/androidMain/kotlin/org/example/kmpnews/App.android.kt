4f775d122aa0ce2f2ca4a0954664dea6d4b6ba2a
Anton Pahodzin <anton.pahodzin@gmail.com>
2024-02-03 20:42:13 +0200

    Add android integration with decompose

--- composeApp/src/androidMain/kotlin/org/example/kmpnews/App.android.kt
+++ composeApp/src/androidMain/kotlin/org/example/kmpnews/App.android.kt
@@ -1,11 +1,15 @@
 package org.example.kmpnews
 
 import android.app.Application
-import android.content.Intent
-import android.net.Uri
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.arkivanov.decompose.defaultComponentContext
+import org.example.kmpnews.helper.initCommon
+import root.component.RootDefaultComponent
 
 class AndroidApp : Application() {
     companion object {
@@ -15,24 +19,20 @@ class AndroidApp : Application() {
     override fun onCreate() {
         super.onCreate()
         INSTANCE = this
+        initCommon()
     }
 }
 
 class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        ) //todo dark mode
+
+        val root = RootDefaultComponent(defaultComponentContext())
         setContent {
-            App()
+            App(root)
         }
     }
-}
-
-internal actual fun openUrl(url: String?) {
-    val uri = url?.let { Uri.parse(it) } ?: return
-    val intent = Intent().apply {
-        action = Intent.ACTION_VIEW
-        data = uri
-        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    }
-    AndroidApp.INSTANCE.startActivity(intent)
 }
--- composeApp/src/androidMain/kotlin/org/example/kmpnews/preview/Preview.kt
+++ composeApp/src/androidMain/kotlin/org/example/kmpnews/preview/Preview.kt
@@ -1,4 +1,4 @@
-package org.example.kmpnews.main
+package org.example.kmpnews.preview
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -6,14 +6,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import main.component.MainComponent
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun MainScreen(
-    component: MainComponent,
-    modifier: Modifier = Modifier
-) {
+@Preview
+fun Preview() {
     Box(modifier = Modifier.fillMaxSize()) {
-        Text("MainScreen", modifier = Modifier.align(Alignment.Center))
+        Text("Hello", modifier = Modifier.align(Alignment.Center))
     }
 }
