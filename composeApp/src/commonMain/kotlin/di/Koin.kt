bbfd55a2922ca9e624a8f6dfdf80f2c5201cfb61
Anton Pahodzin <anton.pahodzin@gmail.com>
2024-02-23 11:17:52 +0200

    Fix the ios target

--- composeApp/src/commonMain/kotlin/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/di/Koin.kt
@@ -3,7 +3,11 @@ package di
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
-fun initKoin(declaration: KoinAppDeclaration = {}) {
+fun initKoin() {
+    initKoin {}
+}
+
+fun initKoin(declaration: KoinAppDeclaration) {
     startKoin {
         declaration()
         modules(dataModule + platformDataModule() + domainModule)
