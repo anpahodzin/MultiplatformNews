d5f3177c50aa97687645fc3fa1731ca38bc99c9d
Anton Pahodzin <anton.pahodzin@gmail.com>
2024-02-27 16:11:45 +0200

    Fix the root component

--- domainModule/src/commonMain/kotlin/root/component/RootComponent.kt
+++ domainModule/src/commonMain/kotlin/root/component/RootComponent.kt
@@ -2,9 +2,10 @@ package root.component
 
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
+import com.arkivanov.essenty.lifecycle.LifecycleOwner
 import main.component.MainComponent
 
-interface RootComponent {
+interface RootComponent: LifecycleOwner {
     val childStack: Value<ChildStack<*, Child>>
 
     sealed class Child {
